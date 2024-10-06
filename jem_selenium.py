bd1d3e83a18e1232f27bd66815485640681c5d32
jemuelbjeroham <jemuelbjeroham@outlook.com>
2024-10-06 14:19:16 +0530

    find_element() test

--- jem_selenium.py
+++ jem_selenium.py
@@ -1,11 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 def test_selenium():
     driver = webdriver.Chrome()
-    driver.get("https://techcraftinator.weebly.com")
-    driver.get("https://techcraftinator.weebly.com/about.html")
-    driver.back()
-    driver.forward()
+    driver.get("https://www.arista.com/en/login")
+    # driver.get("https://techcraftinator.weebly.com/about.html")
+    arista_username = driver.find_element(By.ID, "username")
+    arista_username.send_keys("jemueljeroham.b@hcltech.com")
+    arista_username.send_keys(Keys.RETURN)
+
+    # driver.back()
+    # driver.forward()
     input("Press enter to close the browser... ")
     # driver.quit()
 
